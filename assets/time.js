88c2d373e3251817732a401492d09b21bbc7f8fa
Stanislav Simahin <stopikytub@gmail.com>
2022-09-12 10:43:50 +0300

    Update time.js

--- assets/time.js
+++ assets/time.js
@@ -1,11 +1,7 @@
 window.onload = function() {
      window.setInterval(function() {
           var date = new Date();
-          var hours = date.getHours();    
-          var minutes = date.getMinutes();    
-          var seconds = date.getSeconds();
-          
-          var time = hours + ":" + minutes + ":" + seconds;
+          var time = date.toLocaleTimeString();
           document.getElementById("time").innerHTML = time;
      }, 0);
 }
